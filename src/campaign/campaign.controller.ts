34206c3a493f829e6022632cb2a1528ea4f089cf
Milan Kovac <113798238+milan-kovac@users.noreply.github.com>
2024-10-19 20:15:12 +0200

    Basic db config, created basic modules

---
+++ src/campaign/campaign.controller.ts
@@ -0,0 +1,6 @@
+import { Controller } from '@nestjs/common';
+import { CampaignService } from './campaign.service';
+@Controller('campaigns')
+export class CampaignController {
+  constructor(private readonly campaignService: CampaignService) {}
+}
