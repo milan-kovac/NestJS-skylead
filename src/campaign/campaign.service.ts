6cdfe77511fcc014cd045081f324f0d98e0a8fc9
Milan Kovac <113798238+milan-kovac@users.noreply.github.com>
2024-10-30 20:50:32 +0100

    Added logger decorator and readme.md

--- src/campaign/campaign.service.ts
+++ src/campaign/campaign.service.ts
@@ -4,16 +4,19 @@ import { CreateCampaignRequestDto } from './dtos/request/create.campaign.dto';
 import { User } from '../user/user.entity';
 import { Campaign } from './campaign.entity';
 import { GetAllCampaignsQueryParams } from './dtos/request/paginaion.query.dto';
+import { LogMethod } from '../shared/decorators/log.method.decorator';
 
 @Injectable()
 export class CampaignService {
   constructor(private readonly campaignRepository: CampaignRepository) {}
 
+  @LogMethod()
   async createCampaign(createCampaignRequest: CreateCampaignRequestDto, user: User): Promise<Campaign> {
     const { name } = createCampaignRequest;
     return await this.campaignRepository.create(name, user);
   }
 
+  @LogMethod()
   async getCampaign(id: number, user: User): Promise<Campaign> {
     const campaign = await this.campaignRepository.findById(id, user);
     if (!campaign) {
@@ -22,6 +25,7 @@ export class CampaignService {
     return campaign;
   }
 
+  @LogMethod()
   async getAllCampaigns(user: User, getAllCampaignsQueryParams: GetAllCampaignsQueryParams): Promise<Campaign[]> {
     return await this.campaignRepository.findAll(user, getAllCampaignsQueryParams);
   }
