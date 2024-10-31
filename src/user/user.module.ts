0fa84a14cef0373a99db0372d4bacf5298407a68
AizenAngel <boza.mitrovic@gmail.com>
2024-10-31 16:23:42 +0100

    Remove UserController from user.module.ts

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,12 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserService } from './user.service';
 import { User } from './user.entity';
 import { UserRepository } from './user.respository';
-import { UserController } from './user.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   providers: [UserService, UserRepository],
-  exports: [UserService, UserRepository],
-  controllers: [UserController],
+  exports: [UserService, UserRepository]
 })
 export class UserModule {}
